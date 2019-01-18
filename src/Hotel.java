27d83a0c796d01ce9cff7744eed459d0a53db3e6
Berends <sietze.berends@capgemini.com>
2019-01-18 10:00:28 +0100

    Added part of the framework for the reservations application. Contains a few basic classes in order to create a hotel.

---
+++ src/Hotel.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Hotel {
+
+    // some basic details about the Hotel
+    private String name;
+    private String telNo;
+    private String email;
+
+    private ArrayList<Room> rooms;
+    private ArrayList<Guest> guests;
+    private ArrayList<Invoice> invoices;
+    private ArrayList<Reservation> reservations;
+
+    public Hotel(String name, String telNo, String email) {
+        this.name = name;
+        this.telNo = telNo;
+        this.email = email;
+    }
+}
