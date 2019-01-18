afabd7826383526848883c9517fc2449bbf9714e
sietze-berends <sietze.berends@gmail.com>
2019-01-18 12:42:58 +0100

    Added RoomAttributes class and modified Room class

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,17 @@
+import java.awt.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Currency;
+
 public class Room {
+    private String roomNr;
+    double area;
+    Currency price;
+    int singleBeds;
+    int doubleBeds;
+    int capacity;
+    boolean bookable;
+    ArrayList<RoomAttributes> roomAttributes;
+    ArrayList<String> imageUrls;
+
 }
