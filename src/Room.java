48e1c63232fdf7cb44bd5ea0910225ec25d9e434
sietze-berends <sietze.berends@gmail.com>
2019-01-18 13:29:16 +0100

    Added some variables and constructors for Room and RoomAttributes

--- src/Room.java
+++ src/Room.java
@@ -1,5 +1,3 @@
-import java.awt.*;
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Currency;
 
@@ -14,4 +12,20 @@ public class Room {
     ArrayList<RoomAttributes> roomAttributes;
     ArrayList<String> imageUrls;
 
+    public Room(String roomNr) {
+        this.roomNr = roomNr;
+    }
+
+    public Room(String roomNr, double area, Currency price, int singleBeds, int doubleBeds, int capacity,
+                boolean bookable, ArrayList<RoomAttributes> roomAttributes, ArrayList<String> imageUrls) {
+        this.roomNr = roomNr;
+        this.area = area;
+        this.price = price;
+        this.singleBeds = singleBeds;
+        this.doubleBeds = doubleBeds;
+        this.capacity = capacity;
+        this.bookable = bookable;
+        this.roomAttributes = roomAttributes;
+        this.imageUrls = imageUrls;
+    }
 }
--- src/RoomAttributes.java
+++ src/RoomAttributes.java
@@ -1,2 +1,9 @@
 public class RoomAttributes {
+    private String attributeId;
+    private String description;
+
+    public RoomAttributes(String attributeId, String description) {
+        this.attributeId = attributeId;
+        this.description = description;
+    }
 }
