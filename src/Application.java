d864e593d75432d38ad1a8b9cb26899a742cfe91
Pim Groeneveld <40004027+PimGroeneveld@users.noreply.github.com>
2019-01-18 16:00:17 +0100

    Update Application.java

--- src/Application.java
+++ src/Application.java
@@ -1,16 +1,6 @@
-import java.time.LocalDate;
-import java.util.ArrayList;
-
-public class Application {
-    public static void main(String[] args) {
-<<<<<<< HEAD
-        Hotel molveno = new Hotel("Molveno", "", "");
-=======
-        InitializeHotel initializeHotel = new InitializeHotel();
-        Hotel molveno = initializeHotel.makeMolveno();
->>>>>>> feature/Guest
 import hotel.Hotel;
 import hotel.InitializeHotel;
+
 public class Application {
     public static void main(String[] args) {
 
