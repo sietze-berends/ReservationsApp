716138850144ce0ebeccfbe69c1b04786b811dd0
RosalieVanHassel <46843490+RosalieVanHassel@users.noreply.github.com>
2019-01-24 13:53:56 +0100

    adding reservation functionality and tests, still in progress

---
+++ src/basics/ReservationTest.java
@@ -0,0 +1,70 @@
+package basics;
+
+import hotel.BasicHotel;
+import hotel.Hotel;
+import hotel.InitializeHotel;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ReservationTest {
+    private BasicHotel basicHotel = new BasicHotel();
+    private Reservation res;
+    private Reservation res2;
+
+    @Before
+    public void before(){
+        List<RoomAttribute> room1Attributes = new ArrayList<>();
+        List<RoomAttribute> room2Attributes = new ArrayList<>();
+
+        Room room1 = new Room("101", 20.0, 100.0, 0, 1, 2, true, room1Attributes, null);
+        Room room2 = new Room("102", 40.0, 200.0, 2, 1, 4, true, room2Attributes, null);
+
+        InitializeHotel initializeHotel = new InitializeHotel();
+        Hotel molveno = initializeHotel.makeMolveno();
+        LocalDateTime startDate = LocalDateTime.of(2019,1,22,0,0);
+        LocalDateTime endDate = LocalDateTime.of(2019,1,23,0,0);
+        LocalDateTime startDate2 = LocalDateTime.of(2019,1,25,0,0);
+        LocalDateTime endDate2 = LocalDateTime.of(2019,1,23,0,0);
+        List<Room> roomReservering1 = new ArrayList<>();
+        roomReservering1.add(room1);
+        res = new Reservation(roomReservering1, startDate, endDate, basicHotel.getGuests().get(0) );
+        res2 = new Reservation(roomReservering1, startDate2, endDate2, basicHotel.getGuests().get(0) );
+    }
+
+
+    @Test
+    public void isValid() {
+        //Given
+        // see before
+        //When
+        try {
+            res.isValid();
+        } catch (ReservationException e){
+            Assert.fail("Reservation not valid");
+        }
+    }
+
+    @Test
+    public void isNotValid() {
+        //Given
+        // see before
+        //When
+        try {
+            res2.isValid();
+            Assert.fail("Reservation not valid");
+        } catch (ReservationException e){
+
+        }
+    }
+
+
+
+}
