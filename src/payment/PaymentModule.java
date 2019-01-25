c5df7993178fa920fc7e0b951b7ade279455b7ec
PimGroeneveld <pimgroeneveld92@gmail.com>
2019-01-25 16:13:51 +0100

    comment added to add day aspect of invoice rooms

--- src/payment/PaymentModule.java
+++ src/payment/PaymentModule.java
@@ -8,6 +8,8 @@ public class PaymentModule {
 
     }
 
+    // TODO
+    // add day aspect of rooms (book for 100 eur x 5 days instead of just price)
     public double makeInvoice(Reservation reservation) {
         double totalPrice = 0;
         for (int i = 0; i < reservation.getRooms().size(); i++) {
