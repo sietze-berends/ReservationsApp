7dfbc8ca70ec2e84280ecdb010eda67d4c1986f0
PimGroeneveld <pimgroeneveld92@gmail.com>
2019-01-28 16:08:42 +0100

    controller with /room and /reservation working

--- src/main/java/com/capgemini/molvenohotel/ReserveringsApp/controller/RoomController.java
+++ src/main/java/com/capgemini/molvenohotel/ReserveringsApp/controller/RoomController.java
@@ -3,6 +3,7 @@ package com.capgemini.molvenohotel.ReserveringsApp.controller;
 
 import com.capgemini.molvenohotel.ReserveringsApp.basics.Reservation;
 import com.capgemini.molvenohotel.ReserveringsApp.basics.Room;
+import com.capgemini.molvenohotel.ReserveringsApp.hotel.BasicHotel;
 import com.capgemini.molvenohotel.ReserveringsApp.hotel.Hotel;
 import com.capgemini.molvenohotel.ReserveringsApp.hotel.InitializeReservations;
 import com.capgemini.molvenohotel.ReserveringsApp.hotel.InitializeRooms;
@@ -33,12 +34,12 @@ public class RoomController {
         return initializeRooms.getRooms();
     }
 
-    // werkt nog niet lekker, meer als voorbeeld
-//    @GetMapping(value = "/reservation")
-//    public List<Reservation> getReservations(InitializeReservations initializeReservations) {
-//        return initializeReservations.getReservations();
-//    }
+    @GetMapping(value = "/reservation")
+    public List<Reservation> getReservations(BasicHotel hotel) {
+        return hotel.getReservations();
+    }
 
+    // werkt nog niet lekker, meer als voorbeeld
 //    @GetMapping(value = "/room/{id}")
 //    public List<Room> getRoom(@PathVariable String id){
 //        return searchModule.searchAll(id);
--- src/main/java/com/capgemini/molvenohotel/ReserveringsApp/hotel/InitializeReservations.java
+++ src/main/java/com/capgemini/molvenohotel/ReserveringsApp/hotel/InitializeReservations.java
@@ -1,6 +1,7 @@
 package com.capgemini.molvenohotel.ReserveringsApp.hotel;
 
 import com.capgemini.molvenohotel.ReserveringsApp.basics.Reservation;
+import com.capgemini.molvenohotel.ReserveringsApp.basics.Room;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ public class InitializeReservations {
     private List<Reservation> reservations = new ArrayList<>();
     private Reservation reservation1;
 
+    // vrij zeker dat dit niet correct werkt. Blijf nullpointer expression ontvangen via controller
     public InitializeReservations(Hotel hotel) {
         reservation1 = new Reservation(hotel.getRooms(),
                 LocalDateTime.of(2019, 01, 28, 01, 00),
