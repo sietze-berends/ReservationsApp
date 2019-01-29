ab8bff8313befe547ee1865869c16a33c842de56
PimGroeneveld <pimgroeneveld92@gmail.com>
2019-01-29 15:12:45 +0100

    reservations per booker working

--- src/main/java/com/capgemini/molvenohotel/ReserveringsApp/controller/ReservationController.java
+++ src/main/java/com/capgemini/molvenohotel/ReserveringsApp/controller/ReservationController.java
@@ -1,10 +1,9 @@
 package com.capgemini.molvenohotel.ReserveringsApp.controller;
 
 import com.capgemini.molvenohotel.ReserveringsApp.basics.Reservation;
-import com.capgemini.molvenohotel.ReserveringsApp.hotel.BasicHotel;
 import com.capgemini.molvenohotel.ReserveringsApp.hotel.Hotel;
 import com.capgemini.molvenohotel.ReserveringsApp.hotel.InitializeHotel;
-import com.capgemini.molvenohotel.ReserveringsApp.hotel.Hotel;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,15 +15,18 @@ import java.util.List;
 @RequestMapping(value = "/hotel")
 public class ReservationController {
 
+    private InitializeHotel initializeHotel = new InitializeHotel();
+    private Hotel hotel = initializeHotel.makeMolveno();
+
     //Gets all reservations
     @GetMapping(value = "/reservation")
-    public List<Reservation> getReservations(Hotel hotel) {
+    public List<Reservation> getReservations() {
         return hotel.getReservations();
     }
 
     //Gets reservations per booker
     @GetMapping(value = "/reservation/{bookerId}")
-    public List<Reservation> getReservationByBooker (Hotel hotel, @PathVariable int bookerId){
+    public List<Reservation> getReservationByBooker (@PathVariable int bookerId){
         return hotel.getAllReservationsPerBooker(bookerId);
     }
 
