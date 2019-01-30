a15a2f5b0789bdca54ef79d497a6b9ba01a94e79
PimGroeneveld <pimgroeneveld92@gmail.com>
2019-01-30 16:16:08 +0100

    work in progress on adding room form

---
+++ src/main/resources/static/addRoom.js
@@ -0,0 +1,43 @@
+$('#addRoom').click(function() {
+     // form to add room
+     window.location.href = '/addRoom.html';
+     })
+
+$.ajax({
+  type: "POST",
+  url: url,
+  data: data,
+  success: success,
+  dataType: dataType
+});
+
+
+// open form    
+$(document).on('click', '#addRoom', function(){
+    console.log("test")
+    window.location.href = '/addRoom.html';
+});
+
+// submit the form
+$('#add-room-form').submit(function(event) {
+   var newRoomForm = $(this).serializeArray();
+   var newRoom = {}
+
+   $(newRoomForm).each(function(i, field) {
+       newRoom[field.name] = field.value
+   })
+    console.log(newRoom)
+   $.post( {
+       url : '/hotel/room/add',
+       data : newRoom,
+       succes : function(result) {
+           if(result.validated) {
+               console.log("Room added")
+               }
+           else {
+               console.log("No room added")
+           }
+       }
+   })
+   event.preventDefault();
+})
