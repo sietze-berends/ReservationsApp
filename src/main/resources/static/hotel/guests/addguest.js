706157756850c474f9505df81231b71aad378ba3
PimGroeneveld <pimgroeneveld92@gmail.com>
2019-02-07 11:12:46 +0100

    removes some commented out stuff

--- src/main/resources/static/hotel/guests/addguest.js
+++ src/main/resources/static/hotel/guests/addguest.js
@@ -28,25 +28,3 @@ $("#add-guest").submit(function () {
     });
 
 });
-
-
-// $(document).ready(function(){
-//     // click on button submit
-//     $("#submit").on('click', function(){
-//         // send ajax
-//         $.ajax({
-//             url: '/hotel/allguests/add', // url where to submit the request
-//             type : "POST", // type of action POST || GET
-//             dataType : 'json', // data type
-//             data : $("#add-guest").serialize(), // post data || get data
-//             success : function(result) {
-//                 // you can see the result from the console
-//                 // tab of the developer tools
-//                 console.log(result);
-//             },
-//             error: function(xhr, resp, text) {
-//                 console.log(xhr, resp, text);
-//             }
-//         })
-//     });
-// });
