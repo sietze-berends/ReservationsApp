b13dc72afdf075c98183654e740fcca0436d7e4f
RosalieVanHassel <46843490+RosalieVanHassel@users.noreply.github.com>
2019-02-12 10:35:53 +0100

    reservations
    reservations

--- src/main/resources/static/hotel/reservations/reservations.js
+++ src/main/resources/static/hotel/reservations/reservations.js
@@ -31,8 +31,7 @@ $( document ).ready(function() {
 
         $.each($reservations, function (index, value) {
             content += "<tr>"
-<<<<<<< HEAD
-            content += "<td style='cursor: pointer;' id='reservationNumber' onClick = showSingleReservation()>" + value.reservationNumber + "</td>";
+            content += "<td style='cursor: pointer;' id='reservationNumber' onClick = showSingleReservation()>" + value.id + "</td>";
             content += '<td>' + value.id + '</td>';
             content += '<td>'
             var $rooms = value.roomList
